ccf9cf49c90f651e99af1189a19a969da742b7a5
andyrod <andyrod209@gmail.com>
2022-01-31 19:34:08 -0800

    Created a function to go through each array.

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,7 +1,17 @@
 // list variables in an array
-let destinations = ['San Francisco', 'New York', 'Dallas', "Los Angeles", 'Russia']
-let restaurants = ['Black Bear', 'KFC', 'Olive Garden', 'Pieolegy', 'Boiling Crab', 'WingStop']
-let transportation = ['Rental Car', 'Uber', 'Bus', 'Train','plane', 'horse', 'Lyft']
-let entertainments = ['Movies', 'Concert', 'Walk in Park', 'Bowling', 'Go to the Bar']
+let destinations = ['San Francisco', 'New York', 'Dallas', "Los Angeles", 'Russia'];
+let restaurants = ['Black Bear', 'KFC', 'Olive Garden', 'Pieolegy', 'Boiling Crab', 'WingStop'];
+let transportation = ['Rental Car', 'Uber', 'Bus', 'Train','plane', 'horse', 'Lyft'];
+let entertainments = ['Movies', 'Concert', 'Walk in Park', 'Bowling', 'Go to the Bar'];
 
-// gener
+// generate random numbers\
+function randomPick(trip){    
+    randomNumber = Math.floor(Math.random()* trip.length)
+    
+    console.log(trip[randomNumber])
+    
+}
+randomPick(destinations);
+randomPick(restaurants);
+randomPick(transportation);
+randomPick(entertainments);
