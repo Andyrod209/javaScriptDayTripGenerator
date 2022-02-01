54008f7aae8b968551235896c8ae5eac9a4209fe
andyrod <andyrod209@gmail.com>
2022-01-31 19:07:28 -0800

    Created 4 global variables arrays

---
+++ dayTripGenerator.js
@@ -0,0 +1,7 @@
+// list variables in an array
+let destinations = ['San Francisco', 'New York', 'Dallas', "Los Angeles", 'Russia']
+let restaurants = ['Black Bear', 'KFC', 'Olive Garden', 'Pieolegy', 'Boiling Crab', 'WingStop']
+let transportation = ['Rental Car', 'Uber', 'Bus', 'Train','plane', 'horse', 'Lyft']
+let entertainments = ['Movies', 'Concert', 'Walk in Park', 'Bowling', 'Go to the Bar']
+
+// gener
